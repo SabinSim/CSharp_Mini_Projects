4cc274ffdb968581c5a176449c12e7ab2a0272fd
Sabin Sim <Simgoon89@gmail.com>
2026-03-17 23:46:42 +0100

    RpgLoadoutApp

---
+++ PlayGround/RpgLoadoutApp/RpgLoadoutApp/Items/Item.cs
@@ -0,0 +1,13 @@
+namespace RpgLoadoutApp.Items;
+
+public class Item
+{
+    public int Id { get; }
+    public string Name { get; }
+
+    public Item(int id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+}
